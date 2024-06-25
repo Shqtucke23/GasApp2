3e4d6861477077f202823b06f536133445266bf9
Shawn Tucker <shqtucke@gmail.com>
2024-06-25 18:57:16 -0400

    Initial Commit

---
+++ GasApp2/GasApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  GasApp2App.swift
+//  GasApp2
+//
+//  Created by Shawn Tucker on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GasApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GasApp2UITests/GasApp2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GasApp2UITestsLaunchTests.swift
+//  GasApp2UITests
+//
+//  Created by Shawn Tucker on 6/25/24.
+//
+
+import XCTest
+
+final class GasApp2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
